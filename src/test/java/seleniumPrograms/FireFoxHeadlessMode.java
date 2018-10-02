f49e7704e22efedbc2d8b0a14594044bdd185e42
DeepKandey <deepakggsipu@gmail.com>
2018-10-02 15:50:07 +0530

    Launch Edge Browser checked

--- src/test/java/seleniumPrograms/FireFoxHeadlessMode.java
+++ src/test/java/seleniumPrograms/FireFoxHeadlessMode.java
@@ -1,6 +1,7 @@
 package seleniumPrograms;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -11,7 +12,7 @@ import com.qa.base.TestBase;
 public class FireFoxHeadlessMode extends TestBase {
 
 	@Test
-	public void openBrowserInHeadlessMode() {
+	public void openFirefoxInHeadlessMode() {
 		FirefoxBinary binary = new FirefoxBinary();
 		binary.addCommandLineOptions("--headless");
 
@@ -21,8 +22,20 @@ public class FireFoxHeadlessMode extends TestBase {
 		System.setProperty("webdriver.gecko.driver",
 				"C:\\Users\\deepa\\Downloads\\Drivers\\FireFoxDrivers\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver(options);
+
 		driver.get(
-				"https://confengine.com/selenium-conf-2018/proposal/6220/appium-in-an-enterprise-banking-organisation");
+				"http://learn-automation.com/launch-microsoft-edge-browser-using-selenium-webdriver");
+		System.out.println("Title of the page-->" + driver.getTitle());
+		driver.quit();
+	}
+
+	@Test
+	public void launchEdgeBrowser() {
+		System.setProperty("webdriver.edge.driver",
+				"C:\\Users\\deepa\\Downloads\\Drivers\\EdgeDriver\\MicrosoftWebDriver.exe");
+		WebDriver driver = new EdgeDriver();
+		driver.get(
+				"http://learn-automation.com/launch-microsoft-edge-browser-using-selenium-webdriver");
 		System.out.println("Title of the page-->" + driver.getTitle());
 		driver.quit();
 	}
