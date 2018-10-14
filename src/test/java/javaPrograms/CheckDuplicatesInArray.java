afa3eae4552745387b6908949ed8333dd15639b9
DeepKandey <deepakggsipu@gmail.com>
2018-10-14 10:06:49 +0530

    Class to check Palindrome Number

--- src/test/java/javaPrograms/CheckDuplicatesInArray.java
+++ src/test/java/javaPrograms/CheckDuplicatesInArray.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class CheckDuplicates {
+public class CheckDuplicatesInArray {
 
 	public static void main(String args[]) {
 
