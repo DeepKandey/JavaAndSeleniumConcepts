7a3fbe427a62626a3e6d5d652d680817bbdee71d
DeepKandey <deepakggsipu@gmail.com>
2018-10-29 09:22:21 +0530

    Background color and text color using css added

--- src/test/java/seleniumPrograms/MyFluentWait.java
+++ src/test/java/seleniumPrograms/MyFluentWait.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterMethod;
@@ -31,17 +32,22 @@ public class MyFluentWait extends TestBase {
 	public void fluentWaitCheck() {
 		driver.get("https://www.makemytrip.com/");
 
-		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-				.withTimeout(Duration.ofSeconds(30))
-				.pollingEvery(Duration.ofMillis(500))
-				.ignoring(NoSuchElementException.class);
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofMillis(500)).ignoring(NoSuchElementException.class);
 
 		WebElement hotelsLink = wait.until(new Function<WebDriver, WebElement>() {
 			public WebElement apply(WebDriver driver) {
 				return driver.findElement(By.id("header_tab_hotels"));
 			}
 		});
-
 		System.out.println("Hotels Link is displayed-->" + hotelsLink.getText());
+
+		String backGroundColorSearchBtn = driver.findElement(By.id("searchBtn")).getCssValue("background-color");
+		String textColorSearchBtn = driver.findElement(By.id("searchBtn")).getCssValue("color");
+		System.out.println("Background Color of search button-->" + backGroundColorSearchBtn);
+		System.out.println("Text Color of search button-->" + textColorSearchBtn);
+		System.out.println(
+				"Background Color of search button in Hex-->" + Color.fromString(backGroundColorSearchBtn).asHex());
+		System.out.println("Text Color of search button in Hex-->" + Color.fromString(textColorSearchBtn).asHex());
 	}
 }
