2601c454329be0cb1447ad8fa81fe8d18fe8272d
DeepKandey <deepakggsipu@gmail.com>
2018-11-22 18:11:41 +0530

    issue for entering email id in ticket fare class fixed

--- src/test/java/seleniumPrograms/TrainLineAssignment.java
+++ src/test/java/seleniumPrograms/TrainLineAssignment.java
@@ -16,12 +16,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TrainLineAssignment {
 	WebDriver driver;
-	
+
 	@BeforeTest
 	public void setup() {
-	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\deepa\\Downloads\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
@@ -29,33 +31,40 @@ public class TrainLineAssignment {
 
 	@Test
 	public void verifyTicketFare() throws InterruptedException {
-		JavascriptExecutor js =(JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		driver.get("https://www.thetrainline.com/");
-		
+
 		driver.findElement(By.id("from.text")).sendKeys("London");
 		driver.findElement(By.id("to.text")).sendKeys("Manchester");
-		
+
 		String scriptForDestination = "return document.getElementById(\"to.text\").value;";
-	
-		while(!js.executeScript(scriptForDestination).equals("Manchester")) {
+
+		while (!js.executeScript(scriptForDestination).equals("Manchester")) {
 			driver.findElement(By.id("to.text")).clear();
 			driver.findElement(By.id("to.text")).sendKeys("Manchester");
 		}
-	    driver.findElement(By.id("to.text")).sendKeys(Keys.ENTER);
-	    
-	    WebDriverWait wait = new WebDriverWait(driver, 15);
-	    
-	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-test='submit-journey-search-button']")));		
+		driver.findElement(By.id("to.text")).sendKeys(Keys.ENTER);
+
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[@data-test='submit-journey-search-button']")));
 		driver.findElement(By.xpath("//button[@data-test='submit-journey-search-button']")).click();
-		
+
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("descendant::input[@type='radio'][5]")));
-	  	driver.findElement(By.xpath("descendant::input[@type='radio'][5]")).click();
-			
+		driver.findElement(By.xpath("descendant::input[@type='radio'][5]")).click();
+
 		driver.findElement(By.linkText("Register")).click();
-		
+
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
-		String emailId = RandomStringUtils.randomAlphanumeric(10);
-		driver.findElement(By.id("email")).sendKeys(emailId + "@gmail.com");
+		String randomAlphaNumeric = RandomStringUtils.randomAlphanumeric(10);
+		String completeEmailId = randomAlphaNumeric + "@gmail.com";
+		String[] array = completeEmailId.split("");
+
+		for (String str : array) {
+			driver.findElement(By.name("email")).sendKeys(str);
+		}
+		// driver.findElement(By.name("email")).sendKeys(emailId + "@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("123456");
 		driver.findElement(By.id("firstName")).sendKeys("xyzkl");
 		driver.findElement(By.id("surname")).sendKeys("xyzkl");
@@ -63,45 +72,62 @@ public class TrainLineAssignment {
 
 		Select select = new Select(driver.findElement(By.xpath("//select[@data-test='address-country-dropdown']")));
 		select.selectByVisibleText("India");
-		
+
 		driver.findElement(By.id("addressLine1")).sendKeys("asafkjhf");
 		driver.findElement(By.id("addressLine2")).sendKeys("asafkjhf");
 		driver.findElement(By.id("city")).sendKeys("Mumbai");
 		driver.findElement(By.id("state")).sendKeys("Maharashtra");
 		driver.findElement(By.id("postcode")).sendKeys("411207");
 		driver.findElement(By.xpath("//button[@data-test='register-button']")).click();
-		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-test='submit-journey-search-button']")));		
+
+		wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//button[@data-test='submit-journey-search-button']")));
 		driver.findElement(By.xpath("//button[@data-test='submit-journey-search-button']")).click();
-	  	 
-	  	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("descendant::input[@type='radio'][5]")));
-	  	driver.findElement(By.xpath("descendant::input[@type='radio'][5]")).click();
-			
-	 	String ticketFare=driver.findElement(By.xpath("descendant::input[@type='radio'][5]/parent::div/following-sibling::span/span")).getAttribute("innerHTML");
-		System.out.println("First Class Ticket Fare: " + ticketFare);
-		
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("descendant::input[@type='radio'][5]")));
+		driver.findElement(By.xpath("descendant::input[@type='radio'][5]")).click();
+
+		/*
+		 * String ticketFare = driver .findElement(By.xpath(
+		 * "descendant::input[@type='radio'][5]/parent::div/following-sibling::span/span"
+		 * )) .getAttribute("innerHTML");
+		 */
+		String fareOnMatrixPage = null;
+		try {
+			if (driver
+					.findElement(
+							By.xpath("descendant::input[@type='radio'][5]/parent::div/following-sibling::span/span"))
+					.isDisplayed())
+				fareOnMatrixPage = driver
+						.findElement(By
+								.xpath("descendant::input[@type='radio'][5]/parent::div/following-sibling::span/span"))
+						.getAttribute("innerHTML");
+		} catch (Exception e) {
+			fareOnMatrixPage = driver
+					.findElement(By.xpath(
+							"descendant::input[@type='radio'][5]/parent::div/following-sibling::div/span[2]/span/span"))
+					.getAttribute("innerHTML");
+		}
+		System.out.println("First Class Ticket Fare: " + fareOnMatrixPage);
+
 		Thread.sleep(2000);
-		 
+
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-test='cjs-button-quick-buy']")));
 		driver.findElement(By.xpath("//button[@data-test='cjs-button-quick-buy']")).click();
-			
-		driver.findElement(By.id("email")).sendKeys(emailId + "@gmail.com");
-		driver.findElement(By.id("password")).sendKeys("123456");
-			
-		driver.findElement(By.xpath("//button[@data-test='login-submit-button']")).click();
-			
+
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@data-test='trip-card-total']")));
-		String fareOnCheckout=driver.findElement(By.xpath("//span[@data-test='trip-card-total']/span")).getAttribute("innerHTML");
+		String fareOnCheckout = driver.findElement(By.xpath("//span[@data-test='trip-card-total']/span"))
+				.getAttribute("innerHTML");
 		System.out.println("FareOnCheckout: " + fareOnCheckout);
-		Assert.assertEquals(fareOnCheckout, ticketFare,"Fares do not match");
-		if(fareOnCheckout.equals(ticketFare)) {
+		Assert.assertEquals(fareOnCheckout, fareOnMatrixPage, "Fares do not match");
+		if (fareOnCheckout.equals(fareOnMatrixPage)) {
 			System.out.println("Ticket fares are matching");
 		}
 	}
-	
+
 	@AfterTest
 	public void tearDown() {
-		if(driver!=null)
+		if (driver != null)
 			driver.quit();
 	}
 }
