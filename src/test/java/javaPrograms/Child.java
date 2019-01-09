6c9feb16bbdf57b94436b29c6d3c76a545e219be
DeepKandey <deepakggsipu@gmail.com>
2019-01-09 17:24:40 +0530

    Learnt printing variable and methods using inheritance and run time polymorphism

---
+++ src/test/java/javaPrograms/Child.java
@@ -0,0 +1,20 @@
+package javaPrograms;
+
+public class Child extends Parent {
+
+	int a = 100;
+
+	public void m1() {
+		System.out.println("Child");
+	}
+
+	public static void main(String[] args) {
+		Child obj= new Child();
+		
+		Parent pObj= obj;
+		System.out.println(((Child)pObj).a);
+		System.out.println(pObj.a);
+		pObj.m1();
+	}
+
+}
---
+++ src/test/java/javaPrograms/Parent.java
@@ -0,0 +1,10 @@
+package javaPrograms;
+
+public class Parent {
+
+	int a=10;
+	
+	public void m1() {
+		System.out.println("Parent");
+	}
+}
