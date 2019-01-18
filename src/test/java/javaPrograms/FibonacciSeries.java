3a21064f05871833f4065fd2021c9529e585c11f
DeepKandey <deepakggsipu@gmail.com>
2019-01-18 10:14:21 +0530

    code tuning for fibonacci series

--- src/test/java/javaPrograms/FibonacciSeries.java
+++ src/test/java/javaPrograms/FibonacciSeries.java
@@ -7,47 +7,26 @@ public class FibonacciSeries {
 	private static Scanner scan;
 
 	public static void main(String[] args) {
+		System.out.println("How many numbers you want in Fibonacci:-");
 		scan = new Scanner(System.in);
-		System.out.println("Please enter any number for Fibonacci Series:-");
 		int fibonacciSeriesNumber = scan.nextInt();
 
-		printFibonacciUsingArray(fibonacciSeriesNumber);
-		printFibonacciAtGivenPosition(fibonacciSeriesNumber);
+		printFibonacci(fibonacciSeriesNumber);
 	}
 
-	public static void printFibonacciUsingArray(int number) {
-		int[] fibonacciAraay = new int[number];
+	public static void printFibonacci(int number) {
+		int previousNumber = 0, nextNumber = 1, sum;
+		int fibNumberAtNthPosition = 0;
 
-		if (number == 0) {
-			System.out.println("Entered number should be greater than zero");
-		} else if (number == 1) {
-			fibonacciAraay[0] = 0;
-		} else if (number > 1) {
-			fibonacciAraay[0] = 0;
-			fibonacciAraay[1] = 1;
-			for (int i = 2; i < number; i++) {
-				fibonacciAraay[i] = fibonacciAraay[i - 1] + fibonacciAraay[i - 2];
-			}
+		System.out.println("Fibonacci Series of " + number + " numbers:");
+
+		for (int i = 0; i < number; i++) {
+			System.out.println(previousNumber + " ");
+			fibNumberAtNthPosition = previousNumber;
+			sum = previousNumber + nextNumber;
+			previousNumber = nextNumber;
+			nextNumber = sum;
 		}
-
-		if (number > 0) {
-			System.out.println("---Fibonacci Series---");
-			for (int j = 0; j < number; j++) {
-				System.out.println(fibonacciAraay[j]);
-			}
-		}
-
-	}
-
-	public static void printFibonacciAtGivenPosition(int number) {
-		int a = 0, b = 1, c;
-
-		for (int i = 2; i < number; i++) {
-			c = a + b;
-			a = b;
-			b = c;
-		}
-		if (number > 0)
-			System.out.println("Fibonacci number at position " + number + " is: " + b);
+		System.out.println("Fibonacci number at position " + number + " is: " + fibNumberAtNthPosition);
 	}
 }
