1a74323ea7a6ccbb42cb077e52272b4a8f572b80
DeepKandey <deepakggsipu@gmail.com>
2019-01-19 22:15:03 +0530

    Merge Sort implemented

--- src/test/java/dataStructuresAndAlgorithms/BubbleSort.java
+++ src/test/java/dataStructuresAndAlgorithms/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
 
 	static int[] unsortedArray = { 212, 5, 6, 1, 4, 0, 3, 9 };
 
-	public int[] bubbleSorting(int[] array) {
+	public int[] sort(int[] array) {
 		int size = unsortedArray.length;
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size - i - 1; j++) {
@@ -21,12 +21,14 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BubbleSort obj = new BubbleSort();	
+		
 		System.out.println("--UnSorted Array--");
 		for (int i = 0; i < unsortedArray.length; i++) {
 			System.out.print(unsortedArray[i] + " ");
 		}
 		System.out.println();
-		int[] sortedArray = obj.bubbleSorting(unsortedArray);
+		
+		int[] sortedArray = obj.sort(unsortedArray);
 		System.out.println("--Sorted Array--");
 		for (int i = 0; i < sortedArray.length; i++) {
 			System.out.print(sortedArray[i] + " ");
