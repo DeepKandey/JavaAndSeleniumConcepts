b8fe22495ccbbe39d0a96dce9de6ec4bdc1ceef0
DeepKandey <deepakggsipu@gmail.com>
2019-01-26 16:02:02 +0530

    code to replace a string with another in txt file

---
+++ src/test/java/javaPrograms/ReadFromTextFile.java
@@ -0,0 +1,37 @@
+package javaPrograms;
+
+//Java Program to illustrate reading from FileReader 
+//using BufferedReader 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReadFromTextFile {
+
+	public static void main(String[] args) throws IOException {
+		// We need to provide file path as the parameter:
+		// double back quote is to avoid compiler interpret words
+		// like \test as \t (ie. as a escape sequence)
+
+		File file = new File("C:\\Users\\deepa\\Downloads\\Workflow.txt");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+
+		String st = "";
+		String oldText = "";
+		
+		while ((st = br.readLine()) != null) {
+			oldText = oldText + st + System.lineSeparator();
+			st = br.readLine();
+		}
+		br.close();
+
+		// Replace a word i a file
+		String newText = oldText.replaceAll("workflow", "Deepak");
+
+		FileWriter writer = new FileWriter(file);
+		writer.write(newText);
+		writer.close();
+	}
+}
