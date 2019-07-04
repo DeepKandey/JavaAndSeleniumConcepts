b3c906856827ba44a490681e1e79d85d6dbe8e52
DeepKandey <deepakggsipu@gmail.com>
2019-07-04 23:47:10 +0530

    Generating Screenshot in PDF using itextPDF library

--- src/test/java/seleniumPrograms/CricketScoreboard.java
+++ src/test/java/seleniumPrograms/CricketScoreboard.java
@@ -44,7 +44,6 @@ public class CricketScoreboard {
 						System.out.print(var + "\t\t");
 					}
 				}
-
 			}
 			System.out.println();
 		}
---
+++ src/test/java/seleniumPrograms/GenerateScreenshotPDF.java
@@ -0,0 +1,69 @@
+package seleniumPrograms;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class GenerateScreenshotPDF {
+	WebDriver driver;
+
+	@BeforeTest
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\deepa\\Downloads\\Browser Drivers\\Chrome Drivers\\chromedriver_win.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void screenshotInPDF() throws DocumentException, MalformedURLException, IOException {
+		driver.get("https://www.google.com");
+		// Capture Screenshot and store it in byte[] array format
+		byte[] input = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		Document document = new Document();
+		String output = System.getProperty("user.dir") + "\\Image" + ".pdf";
+		FileOutputStream fos = new FileOutputStream(output);
+
+		// Instantiate the PDF writer
+		PdfWriter writer = PdfWriter.getInstance(document, fos);
+
+		// Open the PDF for Writing
+		writer.open();
+		document.open();
+
+		// Process content into image
+		Image img = Image.getInstance(input);
+
+		// set the size of the image
+		img.scaleToFit(PageSize.A4.getWidth() / 2, PageSize.A4.getHeight() / 2);
+
+		// Add the captured image to PDF
+		document.add(img);
+		document.add(new Paragraph(""));
+
+		// close the files and write to the local systen
+		document.close();
+		writer.close();
+
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+}
