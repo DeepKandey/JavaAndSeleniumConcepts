be1bd2e3776eec7b812eddb4aad3cac8a4f231f6
DeepKandey <deepakggsipu@gmail.com>
2019-09-08 18:13:59 +0530

    little changes in Parralel Browsing for Edge

--- src/test/java/seleniumPrograms/ParallelBrowserHandlingUsingTestNG.java
+++ src/test/java/seleniumPrograms/ParallelBrowserHandlingUsingTestNG.java
@@ -3,7 +3,9 @@ package seleniumPrograms;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -28,8 +30,15 @@ public class ParallelBrowserHandlingUsingTestNG {
 			driver = new FirefoxDriver();
 			System.out.println("Launching " + browserName + " browser");
 		} else if (browserName.equalsIgnoreCase("Edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
+			/*
+			 * System.setProperty("webdriver.edge.driver",
+			 * "C:\\Users\\deepa\\Downloads\\Browser Drivers\\EdgeDriver\\msedgedriver.exe"
+			 * );
+			 */
+			// WebDriverManager.edgedriver().setup();
+			EdgeDriverService service = EdgeDriverService.createDefaultService();
+			driver = new EdgeDriver(service);
+			System.out.println("Launching " + browserName + " browser");
 		}
 	}
 
