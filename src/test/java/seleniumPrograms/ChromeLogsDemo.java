431cec36c48c1abaa9d4fdc34997c45f64ff74d8
DeepKandey <deepakggsipu@gmail.com>
2019-10-13 17:49:45 +0530

    Added classes where browsers logs are stored using selenium

---
+++ src/test/java/seleniumPrograms/ChromeLogsDemo.java
@@ -0,0 +1,27 @@
+package seleniumPrograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLogsDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/deepa/Downloads/Browser Drivers/Chrome Drivers/chromedriver.exe");
+
+		System.setProperty("webdriver.chrome.logfile", "./Chromelog.txt");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().fullscreen();
+
+		driver.get("https://www.google.com/");
+		WebElement googleSearchTxt=driver.findElement(By.name("q"));
+		googleSearchTxt.sendKeys("Hello World");
+		googleSearchTxt.sendKeys(Keys.ARROW_DOWN);
+		googleSearchTxt.sendKeys(Keys.ENTER);
+		driver.quit();
+
+	}
+}
---
+++ src/test/java/seleniumPrograms/FireFoxLogsDemo.java
@@ -0,0 +1,26 @@
+package seleniumPrograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxLogsDemo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver",
+				"C:/Users/deepa/Downloads/Browser Drivers/FireFoxDrivers/geckodriver.exe");
+
+		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "./FFlog.txt");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com/");
+		WebElement googleSearchTxt = driver.findElement(By.name("q"));
+		googleSearchTxt.sendKeys("Hello World");
+		googleSearchTxt.sendKeys(Keys.ARROW_DOWN);
+		googleSearchTxt.sendKeys(Keys.ENTER);
+		driver.quit();
+	}
+}
