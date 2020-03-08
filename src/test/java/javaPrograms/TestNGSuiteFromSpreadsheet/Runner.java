28a52e98829621503c0fc7c4f7691588d39a68b6
DeepKandey <deepakggsipu@gmail.com>
2020-03-08 10:49:03 +0530

    Added class to run test using testNG from executable Jar file

--- src/test/java/javaPrograms/TestNGSuiteFromSpreadsheet/Runner.java
+++ src/test/java/javaPrograms/TestNGSuiteFromSpreadsheet/Runner.java
@@ -6,5 +6,6 @@ public class Runner {
 
 		XLSReaderForXML suite= new XLSReaderForXML("C:\\Users\\deepa\\Downloads\\TestDocument.xlsx");
 		suite.getTests("select * from TestNGxml where module='Order'");
+	
 	} // End of method main
 } // End of class Runner
