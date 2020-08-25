d28b5d9fc7d2144901e785496dda723d5bb96a16
DeepKandey <deepakggsipu@gmail.com>
2020-08-25 08:04:23 +0530

    IPAddress Matcher and Pattern Matcher examples added

---
+++ src/test/java/javaPrograms/PatternMatcher.java
@@ -0,0 +1,41 @@
+/**
+ * @author Deepak Rai
+ */
+package javaPrograms;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * @author Deepak Rai
+ *
+ */
+public class PatternMatcher {
+
+	/**
+	 * {@summary Pattern class example }
+	 * 
+	 * @param
+	 * @return
+	 * @author deepak rai
+	 */
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+
+		while (testCases > 0) {
+			String pattern = in.nextLine();
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (PatternSyntaxException exception) {
+				System.out.println("Invalid");
+			}
+			testCases--;
+		}
+		in.close();
+	}
+
+}
