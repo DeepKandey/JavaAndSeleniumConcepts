8250a3d4a6fb53e51074144d655b84f796ca8f7f
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2021-06-26 14:35:48 +0530

    Annotation Demo

--- src/test/java/javaPrograms/PracticePrograms/Practice.java
+++ src/test/java/javaPrograms/PracticePrograms/Practice.java
@@ -1,9 +1,12 @@
 /* @author Deepak Rai */
 package javaPrograms.PracticePrograms;
 
+import java.io.IOException;
+
 public class Practice {
 
-  public static void main(String[] args) {
-    //
+  public static void main(String[] args) throws IOException {
+
   }
 }
+
