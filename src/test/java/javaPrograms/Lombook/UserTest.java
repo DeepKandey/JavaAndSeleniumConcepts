287d7ec4e645930b84898cda04e6fe571dd820ec
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2021-06-26 07:38:35 +0530

    Lombok builder annotation

--- src/test/java/javaPrograms/Lombook/UserTest.java
+++ src/test/java/javaPrograms/Lombook/UserTest.java
@@ -4,9 +4,15 @@ public class UserTest {
 
   public static void main(String[] args) {
     User user = new User("Shyam", 34, "Haridwar");
-    System.out.println(user.getName() + " " + user.getAge() + " " + user.getAddress());
+    System.out.println(
+        "Original: " + user.getName() + " " + user.getAge() + " " + user.getAddress());
     System.out.println(user.toString());
     user.setAddress("Pune");
-    System.out.println(user.getName() + " " + user.getAge() + " " + user.getAddress());
+    System.out.println(
+        "Updated: " + user.getName() + " " + user.getAge() + " " + user.getAddress());
+
+    User user1 = User.builder().name("Ashish").address("Patna").age(45).build();
+    System.out.println("User with builder Pattern:" + user1);
+    System.out.println("User with no values: " + User.builder().build());
   }
 }
