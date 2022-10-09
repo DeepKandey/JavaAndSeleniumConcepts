5d3126e2421b11779bfe6e3314220d73bf653079
Deepak Kumar Rai <Deepakggsipu@gmail.com>
2022-10-09 11:56:01 +0530

    add max heap delete

--- src/test/java/dataStructuresAndAlgorithms/Heaps/MaxHeap.java
+++ src/test/java/dataStructuresAndAlgorithms/Heaps/MaxHeap.java
@@ -23,20 +23,50 @@ public class MaxHeap {
 
     }
 
+    void deleteFromHeap() {
+        if (size == 0) return;
+
+        int index = 1;
+        array[1] = array[size];
+        size--;
+
+        while (index < size) {
+            int leftIndex = index * 2;
+            int rightIndex = index * 2 + 1;
+
+            if (leftIndex < size && array[leftIndex] > array[index]) {
+                int temp = array[leftIndex];
+                array[leftIndex] = array[index];
+                array[index] = temp;
+                index = leftIndex;
+            } else if (rightIndex < size && array[rightIndex] > array[index]) {
+                int temp = array[rightIndex];
+                array[rightIndex] = array[index];
+                array[index] = temp;
+                index = rightIndex;
+            } else return;
+        }
+    }
+
+
     void print() {
         for (int i = 1; i <= size; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
     }
 
     public static void main(String[] args) {
         MaxHeap maxHeap = new MaxHeap();
+
         maxHeap.insert(50);
         maxHeap.insert(55);
         maxHeap.insert(53);
         maxHeap.insert(52);
         maxHeap.insert(54);
+        maxHeap.print();
 
+        System.out.println();
+        maxHeap.deleteFromHeap();
         maxHeap.print();
     }
 }
